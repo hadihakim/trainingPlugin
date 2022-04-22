bb63cb8372abbd9df722a904fd552f015175426a
hadihakim <hadihakimco@gmail.com>
2022-04-22 20:18:21 +0300

    fixing language tab

--- control/language/language.js
+++ control/language/language.js
@@ -9,20 +9,25 @@ function loadLanguage(lang) {
   });
   stringsUI.init("stringsContainer", strings, stringsConfig);
 }
-loadLanguage("en-us");
 
 function showNewLanguageState(show) {
   if (show) {
-    deleteButton.classList.remove("hidden");
-    saveButton.classList.remove("hidden");
+    //deleteButton.classList.remove("hidden");
+    //saveButton.classList.remove("hidden");
     stringsContainer.classList.remove("hidden");
-    createButton.classList.add("hidden");
+    //createButton.classList.add("hidden");
   } else {
-    deleteButton.classList.add("hidden");
-    saveButton.classList.add("hidden");
+    //deleteButton.classList.add("hidden");
+    //saveButton.classList.add("hidden");
     stringsContainer.classList.add("hidden");
-    createButton.classList.remove("hidden");
+    //createButton.classList.remove("hidden");
   }
+}
+
+async function loadingAndCreateLanguage(lang) {
+  loadLanguage(lang);
+  createLanguage(lang);
+  console.log("Ee");
 }
 
 function createLanguage(language) {
@@ -54,4 +59,22 @@ function save() {
   strings.save(() => {
     buildfire.messaging.sendMessageToWidget({ cmd: "refresh" });
   });
+}
+
+const initialState = async () => {
+  await loadingAndCreateLanguage(langOptions.value);
+}
+
+async function checkInputs() {
+  var inputs = document.getElementsByTagName('input')
+  const events = ['keyup', 'change']
+  for(var j = 0; j < events.length; j++) {
+    for(var i = 0; i < inputs.length; i++) {
+      inputs[i].addEventListener(events[j], e => {
+        save();
+      });
+    }
+  }
+   
+  return true;
 }
