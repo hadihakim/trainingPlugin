96bbf37243f789fcd34f88b266ceaa24b4d1c0f9
hadihakim <hadihakimco@gmail.com>
2022-04-23 00:10:46 +0300

    language cp styling and comment future use + fixing default wysiwyg

--- control/language/language.js
+++ control/language/language.js
@@ -1,33 +1,32 @@
 let strings;
 function loadLanguage(lang) {
   console.log(lang);
-  stringsContainer.classList.add("hidden");
+  //stringsContainer.classList.add("hidden");
   strings = new buildfire.services.Strings(lang, stringsConfig);
   strings.init().then(() => {
-    showNewLanguageState(strings.id);
+    //showNewLanguageState(strings.id);
     strings.inject();
   });
   stringsUI.init("stringsContainer", strings, stringsConfig);
 }
 
-function showNewLanguageState(show) {
+/*function showNewLanguageState(show) {
   if (show) {
     //deleteButton.classList.remove("hidden");
     //saveButton.classList.remove("hidden");
-    stringsContainer.classList.remove("hidden");
+    //stringsContainer.classList.remove("hidden");
     //createButton.classList.add("hidden");
   } else {
     //deleteButton.classList.add("hidden");
     //saveButton.classList.add("hidden");
-    stringsContainer.classList.add("hidden");
+    //stringsContainer.classList.add("hidden");
     //createButton.classList.remove("hidden");
   }
-}
+}*/
 
 async function loadingAndCreateLanguage(lang) {
   loadLanguage(lang);
   createLanguage(lang);
-  console.log("Ee");
 }
 
 function createLanguage(language) {
